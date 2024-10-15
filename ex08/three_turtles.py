a6154e88f6f1c11274b5dbcfe0e37b5a964f6e29
Allyounna <145887785+allyounna@users.noreply.github.com>
2024-10-15 15:58:03 +0700

    Add files via upload

---
+++ ex08/three_turtles.py
@@ -0,0 +1,51 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='turtlesim',
+            namespace='turtlesim1',
+            executable='turtlesim_node',
+            name='sim'
+        ),
+        Node(
+            package='turtlesim',
+            namespace='turtlesim2',
+            executable='turtlesim_node',
+            name='sim'
+        ),
+        Node(
+            package='turtlesim',
+            namespace='turtlesim3',
+            executable='turtlesim_node',
+            name='sim'
+        ),
+        Node(
+            package='turtlesim',
+            executable='mimic',
+            name='mimic12',
+            remappings=[
+                ('/input/pose', '/turtlesim1/turtle1/pose'),
+                ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
+            ]
+        ),
+        Node(
+            package='turtlesim',
+            executable='mimic',
+            name='mimic23',
+            remappings=[
+                ('/input/pose', '/turtlesim2/turtle1/pose'),
+                ('/output/cmd_vel', '/turtlesim3/turtle1/cmd_vel'),
+            ]
+        ),
+        Node(
+            package='teleop_twist_keyboard',
+            executable='teleop_twist_keyboard',
+            name='teleop',
+            prefix='xterm -e',
+            remappings=[
+                ('/cmd_vel', '/turtlesim1/turtle1/cmd_vel'),
+            ]
+        ),
+    ])
